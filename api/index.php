7c866eccaee2288ed7d2ede4e37abcc21d9d041a
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-20 17:16:09 +0200

    add new task

--- api/index.php
+++ api/index.php
@@ -1,6 +1,16 @@
 <?php
 
-$tasks = ["HTML", "CSS", "JavaScript", "PHP"];
+$database_path = __DIR__ . '../database/tasks.json';
+
+$json_data = file_get_contents($database_path);
+
+$tasks = json_decode($json_data, true);
+
+
+$new_task = $_POST('task') ?? null;
+if ($new_task) {
+    $tasks[] = $new_task;
+}
 
 header('Content-Type: application/json');
 
